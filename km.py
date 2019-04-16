069dbeadb4aacd442cdf46afb416e64a46c51dbd
Shivani Shinde <10.shivanishinde@gmail.com>
2019-04-16 14:36:04 +0530

    Add files via upload

--- km.py
+++ km.py
@@ -1,10 +1,20 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 20:34:29 2019
 
-X=[[0.1,0.6],[0.15,0.71],[0.08,0.9],[0.16,0.85],[0.2,0.3],[0.25,0.5],[0.24,0.1],
-   [0.3,0.2]]
-                 
-centers=np.array([[0.1,0.6],[0.3,0.2]])
+@author: Ashwini Bagad
+"""
+
+import numpy as np, pandas as pd
+import matplotlib.pyplot as plt
+         
+data = pd.read_csv('kmns.csv')
+X= np.asarray(data)
+print("X:",X)
+print("Enter indexes between 0 and 7 for cent: ")
+a= int(input())
+b= int(input())
+centers=np.array([X[a],X[b]])
 
 print ('Initial Centroids:')
 print (centers)
@@ -13,7 +23,7 @@ from sklearn.cluster import KMeans
 model=KMeans(n_clusters=2,init=centers,n_init=1)
 model.fit(X)
 print ('\nLabels:', model.labels_)
-print ('\nP6 belongs to cluster :' , model.labels_[5])
+print ('\nP6 belongs to cluster :' , model.labels_[4])
 print ('\nPopulation around cluster 1 :' ,np.count_nonzero(model.labels_==0))
 print ('Population around cluster 2 :' ,np.count_nonzero(model.labels_==1))
 c1=[]
@@ -23,12 +33,17 @@ for i in range(len(model.labels_)):
         c1.append(X[i]) 
     else:
         c2.append(X[i])
+        
 print("Cluster 1:", c1)
 print("Cluster 2:", c2)
 print ('\nNew Centroids are :')
-print (model.cluster_centers_)
+newcent= model.cluster_centers_
+print (newcent)
 plt_c1=np.asarray(c1)
 plt_c2=np.asarray(c2)
+plt.xlabel("X")
+plt.ylabel("Y")
 plt.scatter(plt_c1[:, 0], plt_c1[:, 1])
 plt.scatter(plt_c2[:, 0], plt_c2[:, 1])
+plt.scatter(newcent[0], newcent[1], c='r',s=30)
 plt.show()
